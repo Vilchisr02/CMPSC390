6b71123eb0cd1a7189edf48a03046077297c409f
Eric Cruz <cruz.eric0803@gmail.com>
2025-02-15 11:43:17 -0600

    Implemented User Auth + Code Re-Org

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
+const authRoutes = require('./auth'); // Import the auth routes
 
 const app = express();
 const PORT = 3000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Use the authentication routes
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
