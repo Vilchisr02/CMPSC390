4bbfbd505a92aa5b8984c0bafdc08d3860ed6d3d
Vilchisr02 <vilchis.r02@mymail.sxu.edu>
2025-03-12 15:11:26 -0500

    Update listing.js

--- backend/listing.js
+++ backend/listing.js
@@ -10,7 +10,7 @@ const path = require('path');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'BdIaP1!!',
+    password: 'InsetYourPassWord',
     database: 'eCommerceDB'
 });
 const promisePool = pool.promise();
